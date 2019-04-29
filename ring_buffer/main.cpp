c99547cf33883e1421e4ef3e3f4ee87f29c882e8
Denis Titov <24523794+kelebdil@users.noreply.github.com>
2019-04-29 09:34:19 +0300

    Ring buffer: add test with non-trivial type

--- ring_buffer/main.cpp
+++ ring_buffer/main.cpp
@@ -15,11 +15,11 @@ struct test_ring
     }
 
     template <typename T>
-    void test(std::size_t prefill_items) {
+    void test(std::size_t prefill_items, T (*factory)(std::size_t)) {
         ring_buffer<T> ring(max_arena_size / sizeof(T));
 
         for (std::size_t i = 0; i < prefill_items; ++i) {
-            ring.push(T());
+            ring.push((*factory)(i));
             if (i % 1024 == 0) {
                 std::cout << "Size: " << ring.size() << std::endl;
             }
@@ -31,7 +31,7 @@ struct test_ring
             if (i % 1024 == 0) {
                 std::cout << "Size: " << ring.size() << std::endl;
             }
-            ring.push(T());
+            ring.push((*factory)(i));
         }
 
         std::cout << "Size: " << ring.size() << std::endl;
@@ -40,19 +40,29 @@ struct test_ring
 
 int main(int , char ** )
 {
+    auto int_factory = [](std::size_t i) -> int {
+        return i;
+    };
     {
         test_ring tr;
-        tr.test<int>(max_arena_size/sizeof(int));
+        tr.test<int>(max_arena_size/sizeof(int), +int_factory);
     }
 
     {
         test_ring tr;
-        tr.test<int>(1);
+        tr.test<int>(1, +int_factory);
     }
 
     {
         test_ring tr;
-        tr.test<int>(max_arena_size/sizeof(int)/2);
+        tr.test<int>(max_arena_size/sizeof(int)/2, +int_factory);
+    }
+    auto string_factory = [](std::size_t i) -> std::string {
+        return std::string{"some_preffix"} + std::to_string(i);
+    };
+    {
+        test_ring tr;
+        tr.test<std::string>(max_arena_size/sizeof(std::string), +string_factory);
     }
     return 0;
 }
